c4e131319ba05b3a2c969b9868e4150a42c13e0b
Stefan <bogosavljevic.1911@gmail.com>
2020-05-30 10:25:41 +0200

    feat analitics setup and http client example

---
+++ AnalyticsMicroservice/src/main/java/com/MicroserviceApp/AnalyticsMicroservice/AnalyticsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.MicroserviceApp.AnalyticsMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsMicroserviceApplication.class, args);
+	}
+
+}
